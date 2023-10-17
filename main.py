977c3866a3dc4cbf5eddd78f405ffb9f68e852ec
user <kulakovzena294@gmail.com>
2023-10-17 13:19:59 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('test')
+arr = [[1,3,9], [8,3,5], [5,3,2]]
+averagesArr = []
+averagesDict = {}
+result = []
+
+def calculateAvarega(arr):
+    return (sum(arr) / len(arr))
+
+for i in arr:
+    average = calculateAvarega(i)
+    averagesDict[average] = i
+    averagesArr.append(average)
+
+averagesArr.sort()
+
+for a in averagesArr:
+    result.append(averagesDict[a])
+
+print('Starting array:', arr)
+print('Sorted array by average value:',result)
